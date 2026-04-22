24412fcbcf90f15ffae45a0ee0f5aee3e56e941a
leobarayoyoyo1-creator <leobarayoyoyo1@gmail.com>
2026-04-22 19:38:15 -0300

    versão real

--- versao-wordpress/index.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<?php get_template_part('template-parts/hero'); ?>
-<?php get_template_part('template-parts/about'); ?>
-<?php get_template_part('template-parts/services'); ?>
-<?php get_template_part('template-parts/partners'); ?>
-<?php get_template_part('template-parts/testimonials'); ?>
-<?php get_template_part('template-parts/contact'); ?>
-<?php get_footer(); ?>
